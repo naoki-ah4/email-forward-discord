107bf70a2bfcc4671ed1b67000b7b9de848776c5
aho4ahoaho <naoki-ah4@users.noreply.github.com>
2025-06-28 22:26:24 +0900

    feat: add @types/node and undici-types dependencies; refactor email handling to improve Discord message formatting

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import PostalMime from "postal-mime";
+import PostalMime, { Email } from "postal-mime";
+import { createHash } from "node:crypto";
 
 export default {
 	async email(message, env, ctx) {
 		const webhookUrl = env.DISCORD_WEBHOOK_URL;
 		const email = await PostalMime.parse(message.raw)
-		const subject = email.subject || message.headers.get('subject')
-		const body = email.text || "本文なし"
-		const data = {
-			"content": `送信元:${message.from}\n宛先:${message.to}\n件名:${subject || "件名なし"}\n\n${body}`
-		}
+		const data = buildDiscordContent(email, message);
+
 		const res = await fetch(webhookUrl, {
 			method: "POST",
 			headers: {
@@ -33,3 +31,31 @@ export default {
 		}
 	}
 } satisfies ExportedHandler<Env>;
+
+const buildDiscordContent = (email: Email, message: ForwardableEmailMessage) => {
+	const subject = email.subject || message.headers.get('subject')
+	const body = email.text || "本文なし"
+	const emailFrom = email.from.address || message.from.toString() || "不明";
+	const emailTo = email.to?.join(",") || message.to.toString() || "不明";
+
+	const md5Hash = email && createHash("md5").update(emailFrom).digest("hex");
+	const iconUrl =
+		md5Hash && `https://www.gravatar.com/avatar/${md5Hash}?d=identicon`;
+
+	const data = {
+		"avarar_url": iconUrl,
+		"username": `${email.from.name || "不明"} <${email.from.address || "不明"}>`,
+		"embeds": [
+			{
+				"title": subject || "件名なし",
+				"description": body || "本文なし",
+				"color": 0x00ff00, // 緑色
+				"footer": {
+					"text": `From: ${emailFrom} To: ${emailTo}`,
+				},
+				"timestamp": new Date().toISOString()
+			}
+		]
+	}
+	return data;
+}
